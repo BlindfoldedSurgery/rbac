bb5315d1038e803768c384a67f4a161e45f36dbd
Torben Carstens <git@carstens.tech>
2023-02-19 12:40:46 +0100

    Initial commit

---
+++ get-kubeconfig.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+namespace=$1
+secretname=$2
+filename=$(openssl rand -hex 12).yml
+
+# TOKEN=$(kubectl -n $namespace create token $serviceaccount)
+TOKEN=$(kubectl -n $namespace get secret -o json $2 | jq -r '.data.token')
+
+kubectl config view --flatten --minify > $filename
+
+export KUBECONFIG=$(pwd)/$filename 
+kubectl config set-credentials $2 --token=$TOKEN
+kubectl config set-context --user=$2 --current
+kubectl config view --flatten --minify > $serviceaccount.config
+
+rm $filename
+
+/usr/bin/cat $serviceaccount.config
